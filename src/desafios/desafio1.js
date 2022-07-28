36dd5dd8b65bc8ecdc845f49d2c59a96ad3ee5e8
Luiz Augusto <lugaasjbh@gmail.com>
2022-07-28 15:08:43 -0300

    Update desafio1.js

--- src/desafios/desafio1.js
+++ src/desafios/desafio1.js
@@ -1,5 +1,28 @@
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+// Como podemos rodar isso em um arquivo .ts sem causar erros?
 
-let employee = {};
-employee.code = 10;
-employee.name = "John";
+// resposta 1
+let employee = {
+  code = 10;
+  name = "John";
+}
+
+// resposta 2
+const employee: {code: number, name: string} = {
+  code: 10
+  name: 'john'
+}
+
+// resposta 3 // Sobre interfaces consulte blog.logrocket.com/types-vs-interfaces-in-typescript/
+interface Employee {  
+    code: number,
+    name: string,
+};
+
+const employeeObj = {} as Employee;
+  employeeObj.codigo = 10;
+  employeeObj.nome = 'john';
+
+const employeeObj2: Employee = {
+  code: 10,
+  name: 'john'
+}
